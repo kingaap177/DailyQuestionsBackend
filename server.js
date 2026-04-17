70a9359cc9048aceb89abbd326c13275b40f3cb6
kingaap177 <93843288+kingaap177@users.noreply.github.com>
2026-04-17 13:35:49 +0200

    start

---
+++ server.js
@@ -0,0 +1,23 @@
+// const express = require('express');
+// const app = express();
+
+// const PORT = 3000;
+
+// // Middleware (so you can read JSON from requests)
+// app.use(express.json());
+
+// // Test route
+// app.get('/', (req, res) => {
+//   res.send('Hello from your backend 🚀');
+// });
+
+// // Example API route
+// app.get('/api/data', (req, res) => {
+//   res.json({ message: 'This is your API data' });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+
